3231a2f51c792627e387951b29601512c55571e3
Victor MN <minascurtavictor.r@gmail.com>
2025-05-26 08:38:48 +0300

    fix macos apple clang 15

--- src/Bot.cpp
+++ src/Bot.cpp
@@ -4,8 +4,8 @@ bool Bot::joacaAtu(const Culoare c) {
     return atu_strategy->jucare(c, hand);
 }
 
-Culoare Bot::alegeAtu(int id) {
-    return atu_strategy->alegere(*getPHand(), id, this->id);
+Culoare Bot::alegeAtu(const int id1) {
+    return atu_strategy->alegere(*getPHand(), id1, this->id);
 }
 
 Carte Bot::joaca(std::vector<Carte>& pe_masa, const bool atu_free, Puncte& pct, const Culoare atu, const int decl) {
